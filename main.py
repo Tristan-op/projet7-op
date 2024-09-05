269a67faebddd63af095244976cf3e879c640483
Tristan-op <tristan.brioulet@gmail.com>
2024-09-05 10:02:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -55,4 +55,5 @@ def exit_application():
     return "Merci d'avoir utilisé notre service. L'application est fermée.", 200
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
+
