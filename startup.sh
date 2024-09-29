31afc62e24f3841c28b4ef7a5b1354df21905f77
Tristan-op <tristan.brioulet@gmail.com>
2024-09-29 20:41:58 +0200

    Delete startup.sh

--- startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-pip install spacy
-python -m spacy download en_core_web_sm
-
-# Utiliser la variable d'environnement PORT définie par Azure, sinon utiliser le port 80
-PORT=${PORT:-80}
-
-gunicorn --bind 0.0.0.0:$PORT main:app
