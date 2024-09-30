46032231b11d0f3d9032a99ee9ba6f699cdffcd2
Tristan-op <tristan.brioulet@gmail.com>
2024-09-30 12:14:44 +0200

    update

--- startup.sh
+++ startup.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-# Mise à jour de pip
-echo "Mise à jour de pip..."
-pip install --upgrade pip
-
-# Installation des dépendances à partir du fichier requirements.txt
+# Installation des dépendances définies dans requirements.txt
 echo "Installation des dépendances..."
+pip install --upgrade pip
 pip install -r requirements.txt
 
-# Lancer l'application avec Gunicorn
-echo "Lancement de l'application avec Gunicorn..."
+# Lancer l'application Flask avec Gunicorn sur le port spécifié
+if [ -z "$PORT" ]; then
+  PORT=8000
+fi
+
+echo "Lancement de l'application avec Gunicorn sur le port $PORT..."
 gunicorn --bind 0.0.0.0:$PORT main:app
