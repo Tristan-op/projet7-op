5373257e8f22ac9c5a53fc3df7b54a12f4e3b43f
Tristan-op <tristan.brioulet@gmail.com>
2024-09-30 12:29:00 +0200

    update

--- startup.sh
+++ startup.sh
@@ -5,3 +5,5 @@ echo "Installation des dépendances..."
 pip install --upgrade pip
 pip install -r requirements.txt
 
+echo "Lancement de l'application avec Gunicorn..."
+gunicorn --bind 0.0.0.0:$PORT main:app &
