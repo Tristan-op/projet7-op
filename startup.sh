bc96fb3adcb5ea83ba46576e251cd66ed561c266
Tristan-op <tristan.brioulet@gmail.com>
2024-09-30 16:34:56 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -11,16 +11,6 @@ import tensorflow as tf
 import spacy
 import gensim.downloader as api
 
-# Test d'importation réussi
-print("Importation des modules réussie.")
-
-# Initialiser spaCy
-nlp = spacy.load('en_core_web_sm')
-print("Modèle spaCy initialisé.")
-
-# Charger un modèle TensorFlow (si applicable)
-lstm_model = tf.keras.models.load_model('./models/LSTM_plus_Lemmatization_plus_FastText_model.h5')
-print("Modèle TensorFlow chargé.")
 END
 
 echo "Lancement de l'application avec Gunicorn..."
