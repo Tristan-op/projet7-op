3de5b1687f6bca950265c5fd3bb8b91e5ee0b33b
Tristan-op <tristan.brioulet@gmail.com>
2024-09-30 17:00:24 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "Installation des dépendances..."
+pip install --upgrade pip
+pip install -r requirements.txt
+
 echo "Importation des modules pour les tests..."
 python - << END
 import numpy as np
@@ -10,4 +14,4 @@ import gensim.downloader as api
 END
 
 echo "Lancement de l'application avec Gunicorn..."
-
+gunicorn --bind 0.0.0.0:$PORT main:app
