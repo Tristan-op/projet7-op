59ee7c0af71e19c207980028cb25cec7327ffdd5
Tristan-op <tristan.brioulet@gmail.com>
2024-09-30 11:44:34 +0200

    up

--- startup.sh
+++ startup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-echo "Installation des dépendances..."
+
+# Mise à jour de pip
+echo "Mise à jour de pip..."
 pip install --upgrade pip
+
+# Installation des dépendances à partir du fichier requirements.txt
+echo "Installation des dépendances..."
 pip install -r requirements.txt
 
-echo "Vérification de l'installation de numpy..."
-pip show numpy
-
+# Lancer l'application avec Gunicorn
 echo "Lancement de l'application avec Gunicorn..."
+gunicorn --bind 0.0.0.0:$PORT main:app
