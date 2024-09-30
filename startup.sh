25dc5500d49e3344aae6b1f866dc30ba93f837b5
Tristan-op <tristan.brioulet@gmail.com>
2024-09-30 15:25:12 +0200

    update

--- startup.sh
+++ startup.sh
@@ -14,17 +14,7 @@ import gensim.downloader as api
 # Test d'importation réussi
 print("Importation des modules réussie.")
 
-# Charger le modèle FastText pour tester l'importation
-ft_model = api.load('fasttext-wiki-news-subwords-300')
-print("Modèle FastText chargé.")
 
-# Initialiser spaCy
-nlp = spacy.load('en_core_web_sm')
-print("spaCy initialisé.")
-
-# Charger un modèle TensorFlow pour tester l'importation (si applicable)
-# lstm_model = tf.keras.models.load_model('./models/LSTM_plus_Lemmatization_plus_FastText_model.h5')
-# print("Modèle TensorFlow chargé.")
 END
 
 echo "Lancement de l'application avec Gunicorn..."
