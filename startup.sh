8aa03d8685577eecbf02a79e6bcfcde4cea781ee
Tristan-op <tristan.brioulet@gmail.com>
2024-10-13 08:26:41 +0200

    update

--- startup.sh
+++ startup.sh
@@ -10,6 +10,16 @@ import numpy as np
 import tensorflow as tf
 import spacy
 import gensim.downloader as api
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+
+# Télécharger le modèle spaCy en_core_web_sm si nécessaire
+try:
+    spacy.load('en_core_web_sm')
+except OSError:
+    spacy.cli.download('en_core_web_sm')
+
+# Charger le modèle FastText avec Gensim
+api.load('fasttext-wiki-news-subwords-300')
 
 END
 
